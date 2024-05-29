62847f98963dc441d198f3b2d342e3ee24ff71d4
Aman Sinha <aman.s@technovert.com>
2024-05-29 17:00:37 +0530

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UpdateBoxComponent } from './side-bar/update-box/update-box.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,UpdateBoxComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
